959c91690cea9638eb548d9e1515a8e947c21b54
aleprosli <aliff.rosli96@gmail.com>
2025-08-18 15:43:06 +0800

    store route

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/tempahan/create', [App\Http\Controllers\TempahanController::class, 'create'])->name('tempahan.create');
+Route::post('/tempahan/store', [App\Http\Controllers\TempahanController::class, 'store'])->name('tempahan.store');
