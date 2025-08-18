2f98c1b27d005299afde675bd8319564394dd808
aleprosli <aliff.rosli96@gmail.com>
2025-08-18 15:42:59 +0800

    STORE

--- app/Http/Controllers/TempahanController.php
+++ app/Http/Controllers/TempahanController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tempahan;
 
 class TempahanController extends Controller
 {
     public function create()
     {
         return view('tempahan.create');
+    }
+
+    public function store(Request $request)
+    {
+        Tempahan::create([
+            'nama_penuh' => $request->nama_penuh,
+            'bilik_makmal' => $request->bilik_makmal,
+            'tarikh' => $request->tarikh
+        ]);
+
+        return redirect()->route('home');
     }
 }
--- resources/views/tempahan/create.blade.php
+++ resources/views/tempahan/create.blade.php
@@ -8,18 +8,19 @@
                 <div class="card-header">Borang Tempahan Makmal</div>
 
                 <div class="card-body">
-                    <form>
+                    <form method="POST" action="{{ route('tempahan.store') }}">
+                        @csrf
                         <div class="mb-3">
                           <label for="nama_penuh" class="form-label">Nama Penuh</label>
-                          <input type="text" class="form-control">
+                          <input type="text" class="form-control" name="nama_penuh">
                         </div>
                         <div class="mb-3">
                           <label for="bilik_makmal" class="form-label">Bilik Makmal</label>
-                          <input type="text" class="form-control" id="bilik_makmal">
+                          <input type="text" class="form-control" id="bilik_makmal" name="bilik_makmal">
                         </div>
                         <div class="mb-3">
                             <label for="bilik_makmal" class="form-label">Tarikh</label>
-                            <input type="date" class="form-control">
+                            <input type="date" class="form-control" name="tarikh">
                           </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                       </form>
